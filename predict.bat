d7e70874e2baaaf3a9adb9b277b5daec2dfc3ac2
smtydn <sametaydin1997@gmail.com>
2021-05-08 18:18:49 +0300

    bat script for prediction

--- predict.bat
+++ predict.bat
@@ -3,6 +3,6 @@ ECHO Prediction!
 python main.py predict ^
     --weights_path="C:\Users\samet\Projects\fast_style_transfer\weights\rain_princess.h5" ^
     --save_path="C:\Users\samet\Projects\fast_style_transfer\images\outputs\output.png" ^
-    --image_path="C:\Users\samet\Projects\fast_style_transfer\images\content\stata.jpg"
+    --image_path="C:\Users\samet\Projects\fast_style_transfer\images\content\chicago.jpg"
 
 PAUSE
